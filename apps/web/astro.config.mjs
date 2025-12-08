1ad95b2c20e14c6154646b52e97218e1e3f695d5
Onur Ardıç <onurardc@outlook.com>
2025-12-08 20:59:19 +0300

    aynen aynen

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,16 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
+import node from "@astrojs/node";
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
+
   integrations: [svelte(), react()],
   output: "server",
+
   vite: {
     resolve: {
       alias: {
@@ -16,8 +21,8 @@ export default defineConfig({
       preprocessorOptions: {
         scss: {
           additionalData: `
-           @use "@/styles/global.scss" as *;
-           @use "@/styles/variables.scss" as *;
+            @use "@/styles/global.scss" as *;
+            @use "@/styles/variables.scss" as *;
           `,
         },
       },
