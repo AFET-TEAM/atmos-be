cc4773af38fd7e5808fea9ff4c23db0b898aa4d7
Onur Ardıç <onurardc@outlook.com>
2025-12-08 20:47:01 +0300

    feat :Added hostname

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -48,5 +48,5 @@ const v1 = new Elysia({ prefix: "/v1", name: "api:v1" })
   .use(cityRoutes());
 app.use(v1);
 
-app.listen({ port: 3000 });
+app.listen({ port: 3000, hostname: "0.0.0.0" });
 console.log("Elysia on http://localhost:3000 | API: /v1 | Swagger: /swagger");
