eb9e26373aa7a988784022a710a4fda5846f92b4
Onur Ardıç <onurardc@outlook.com>
2025-12-08 21:08:39 +0300

    Fix : fixed import path

--- apps/web/src/components/Techtalks/TechtalkSideList/TechtalkSideList.svelte
+++ apps/web/src/components/Techtalks/TechtalkSideList/TechtalkSideList.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+  import Icon from "../../UI/Icon.svelte";
   import type { TechTalk } from "../types/TechTalks";
-  import Icon from "../../UI/Icon.svelte"
-  import "./TechTalkSideList.scss";
+  import "./TechtalkSideList.scss";
 
   export let items: TechTalk[] = [];
   export let activeId: number | undefined;
@@ -23,7 +23,7 @@
         class="tt-side__item {activeId === o.id ? 'is-active' : ''}"
         href={`/techtalks/${o.id}`}
         title={o.title}
-        aria-current={activeId === o.id ? 'page' : undefined}
+        aria-current={activeId === o.id ? "page" : undefined}
       >
         <div class="tt-side__thumb">
           {#if o.thumbnailUrl}
@@ -33,23 +33,25 @@
           {/if}
         </div>
 
-       <div class="tt-side__info">
-  <div class="tt-side__title">{o.title}</div>
+        <div class="tt-side__info">
+          <div class="tt-side__title">{o.title}</div>
 
-  <div class="tt-side__meta">
-    <span class="meta-item" title="Sahip">
-      <Icon name="owner" width={14} height={14}  />
-      <span class="text">{o.owner}</span>
-    </span>
+          <div class="tt-side__meta">
+            <span class="meta-item" title="Sahip">
+              <Icon name="owner" width={14} height={14} />
+              <span class="text">{o.owner}</span>
+            </span>
 
-    {#if (o as any).date}
-      <span class="meta-item" title="Tarih">
-         <Icon name="date" width={14} height={14}  />
-        <span class="text">{fmt((o as any).date) ?? (o as any).date}</span>
-      </span>
-    {/if}
-  </div>
-</div>
+            {#if (o as any).date}
+              <span class="meta-item" title="Tarih">
+                <Icon name="date" width={14} height={14} />
+                <span class="text"
+                  >{fmt((o as any).date) ?? (o as any).date}</span
+                >
+              </span>
+            {/if}
+          </div>
+        </div>
       </a>
     {/each}
   {/if}
