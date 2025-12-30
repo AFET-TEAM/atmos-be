9f750a105ed48ed016b945a677e08e72599f5940
Onur Ardıç <onurardc@outlook.com>
2025-12-31 01:09:47 +0300

    Fix : fixed scss import

--- apps/web/src/components/Techtalks/TechtalkSideList/TechtalkSideList.svelte
+++ apps/web/src/components/Techtalks/TechtalkSideList/TechtalkSideList.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
+  import Icon from "../../UI/Icon.svelte";
   import type { TechTalk } from "../types/TechTalks";
-  import Icon from "../../UI/Icon.svelte"
-  import "./TechTalkSideList.scss";
+  import "./TechtalkSideList.scss";
 
   export let items: TechTalk[] = [];
   export let activeId: number | undefined;
-  
+
   const fmt = (d?: string) => {
     if (!d) return null;
     const date = new Date(d);
@@ -18,14 +18,12 @@
   {#if items.length === 0}
     <div class="tt-side__empty">Diğer techtalk bulunamadı.</div>
   {:else}
-  
     {#each items as o (o.id)}
-    
       <a
         class="tt-side__item {activeId === o.id ? 'is-active' : ''}"
         href={`/techtalks/${o.id}`}
         title={o.title}
-        aria-current={activeId === o.id ? 'page' : undefined}
+        aria-current={activeId === o.id ? "page" : undefined}
       >
         <div class="tt-side__thumb">
           {#if o.thumbnailUrl}
@@ -35,23 +33,25 @@
           {/if}
         </div>
 
-       <div class="tt-side__info">
-  <div class="tt-side__title">{o.title}</div>
+        <div class="tt-side__info">
+          <div class="tt-side__title">{o.title}</div>
 
-  <div class="tt-side__meta">
-    <span class="meta-item" title="Sahip">
-      <Icon name="owner" width={14} height={14}  />
-      <span class="text">{o.owner}</span>
-    </span>
+          <div class="tt-side__meta">
+            <span class="meta-item" title="Sahip">
+              <Icon name="owner" width={14} height={14} />
+              <span class="text">{o.owner}</span>
+            </span>
 
-    {#if (o as any).date}
-      <span class="meta-item" title="Tarih">
-         <Icon name="clock" width={14} height={14}  />
-        <span class="text">{fmt((o as any).date) ?? (o as any).date}</span>
-      </span>
-    {/if}
-  </div>
-</div>
+            {#if (o as any).date}
+              <span class="meta-item" title="Tarih">
+                <Icon name="clock" width={14} height={14} />
+                <span class="text"
+                  >{fmt((o as any).date) ?? (o as any).date}</span
+                >
+              </span>
+            {/if}
+          </div>
+        </div>
       </a>
     {/each}
   {/if}
